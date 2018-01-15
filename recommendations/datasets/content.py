51890f68e5628aa029322f9859304e0febfadde6
Sidharth Shah <sidharth@fafadiatech.com>
2018-01-15 14:14:08 +0530

    fix for bug, we were downloading file but not uncompressing it. added missing requirements aswell

--- recommendations/datasets/content.py
+++ recommendations/datasets/content.py
@@ -11,7 +11,7 @@ class NewsDataset(Dataset):
     def __init__(self, max_sample=0):
         self._name = "UCI News Dataset"
         self._download_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00359/NewsAggregatorDataset.zip"
-        self.download()
+        self.download_and_unzip()
         self.download_post_process(max_sample)
 
     def download_post_process(self, max_sample=0):
